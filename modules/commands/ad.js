bcd5a3452dab8de6ee5f849febe5d2a8af618bee
trin122 <80610907+trin122@users.noreply.github.com>
2024-02-24 19:53:09 +0700

    Update ad.js

--- modules/commands/ad.js
+++ modules/commands/ad.js
@@ -1,76 +1,43 @@
-const request = require('request');
-
-const fs = global.nodemodule["fs-extra"]
-
 module.exports.config = {
-
   name: "adbot",
-
   version: "1.0.0",
-
   hasPermssion: 0,
-
-  credits: "SINGU-💌💌",
-
-  description: "Kiểm tra thông tin adminbot",
-
-  commandCategory: "Hệ Thống",
-
-  usages: "adbot",
-
+  credits: "",
+  description: "Kiểm tra thông tin admin .",
+  commandCategory: "Admin",
+  usages: "adm",
   cooldowns: 0,
-
   dependencies: {
-
-"request": ""
-
-}
-
+    "request":"",
+    "fs-extra":"", 
+    "axios":""
+  }
 };
-module.exports.run = async({api,event,args,Users,global,Currencies}) => {
 
-var callback = () => api.sendMessage(
-
-  {body:`[💌]=== 『 INFORMATION ADMIN 』 ===[💌]
-◆━━━━━━━━━━━━━━━━◆
-
-
-[👀]➜ Tên: 𝐍𝐠𝐮𝐲𝐞̂̃𝐧 𝐇𝐮̛̃𝐮 𝐓𝐫𝐢𝐧 (trình)
-[💮]➜ Biệt danh:  
-[❎]➜ Ngày tháng năm sinh: 19/05/20xx
-[👤]➜ Giới tính: Nam
-[💘]➜ Mối quan hệ: Độc toàn thân
-[🌎]➜ Quê quán: Sao Hoả 
-[🌸]➜ Tính cách: Hòa đồng, năng nổ, dứt khoát, thân thiện và hài hước
-[🌀]➜ Sở thích: GYM,Football,Chơi gái
-
-[💌]=== 『 CONTACT 』 ===[💌]
-◆━━━━━━━━━━━━━━━━◆
-
-[👉]➜ Information: BOTSEGS 
-[☎]➜ SĐT & Zalo: 086998xxxx
-0[🌐]➜ Facebook: https://www.facebook.com/htrin.1905
-[✉️]➜ Email: huutrinnguyen@gmail.com
-
-[💌]=== 『 PROBLEM 』 ===[💌]
-◆━━━━━━━━━━━━━━━━◆
-
-
-[❗]➜ Mọi thắc mắc hay bot không hoạt động có thể hỏi trực tiếp admin hoặc dùng /callad
-[📌]➜ Hãy đồng hành cùng BOT và mình nhé. Cảm ơn mọi người <3
-
-✎﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏﹏
-
-[📝]➜ Bot được điều hành bởi admin bot`,
-
-    attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
-
-    fs.unlinkSync(__dirname + "/cache/1.png"));  
-
-      return request(
-
-        encodeURI(`https://graph.facebook.com/${100000194247253}/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(
-
-fs.createWriteStream(__dirname+'/cache/1.png')).on('close',() => callback());
-
-       };
+module.exports.run = async({api,event,args,client,Users,Threads,__GLOBAL,Currencies}) => {
+const axios = global.nodemodule["axios"];
+const request = global.nodemodule["request"];
+const fs = global.nodemodule["fs-extra"];
+  var link = [
+"https://i.imgur.com/HDXcUN2.mp4",
+"https://i.imgur.com/lzIZXcf.jpg"
+    
+  ];
+  var callback = () => api.sendMessage({body:`🌺梁ADMIN梁🌺
+  👀 Tên: 𝑵𝒈𝒖𝒚𝒆̂̃𝒏 𝑯𝒖̛̃𝒖 𝑻𝒓𝒊𝒏 (Trình)
+💮 Biệt danh: Không có
+❎ Tuổi: 20xx
+👤 Giới tính: Nam
+💘 Mối quan hệ: ế lòi con cu
+🌎 Quê quán: ??
+👫 Gu: Ghệ đích bự
+🌸 Tính cách: trung thực 100%
+🌀 Sở thích: đạo dụ
+💻Contact💻
+☎ SĐT&Zalo:09167?????
+🌐 https://www.facebook.com/htrin.1905
+✉️ Email:huutrinnguyen@gmail.com
+🌸Lưu ý : 『 𝙆𝙝ô𝙣𝙜 𝙨𝙥𝙖𝙢 𝙗𝙤𝙩,𝙆𝙝ô𝙣𝙜 𝙘𝙝ử𝙞 𝙗𝙤𝙩,𝙆𝙝ô𝙣𝙜 𝙠𝙞𝙘𝙠 𝙗𝙤𝙩 𝙩ù𝙮 𝙩𝙞ệ𝙣 𝙠𝙞𝙘𝙠 𝙣𝙝ớ 𝙣ó𝙞 𝙖𝙙𝙢,𝙎𝙥𝙖𝙢 𝙗𝙤𝙩 𝙩ự 𝙗𝙖𝙣,𝙆𝙝ô𝙣𝙜 𝙧𝙚𝙥𝙤𝙧𝙩 𝙖𝙘𝙘 𝙗𝙤𝙩』
+`,attachment: fs.createReadStream(__dirname + "/cache/5.mp4")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/5.mp4")); 
+      return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/5.mp4")).on("close",() => callback());
+   };
